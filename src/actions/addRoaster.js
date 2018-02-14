92d150ac300730f20206317aa4eb68dae6a7d2c5
Amy Russell <russell.amyh@gmail.com>
2018-02-14 16:43:54 -0700

    initial commit

---
+++ src/actions/addRoaster.js
@@ -0,0 +1,8 @@
+export const addRoaster( (name, location, contact, equipment, water) => {
+  type: 'ADD_ROASTER',
+  name,
+  location,
+  contact,
+  equipment,
+  water
+})
---
+++ src/components/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React { Component }from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class CardContainer extends Component {
+
+  render() {
+    return(
+      <div>
+        <h1>Coffee Roaster name</h1>
+      </div>
+      )
+  }
