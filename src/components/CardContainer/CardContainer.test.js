893f444d17ce7a81e0bb8d32ac635350cdf64457
Amy Russell <russell.amyh@gmail.com>
2018-02-24 11:32:21 -0700

    passing snapshot test for card container

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -1,1 +1,32 @@
 // needs a snapshot test and mstp/mdtp
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CardContainer } from './CardContainer';
+import { shallow } from 'enzyme';
+
+describe('CardContainer', () => {
+  it('should render correctly', () => {
+    const mockRoasters = [
+      { userId: "71yYVDRUpOO8BR5153LMNCOmrV52",
+        name: "Corvus",
+        location: "Denver",
+        altitude: "5280",
+        equipment: "drum",
+        water: "calcium",
+        contact: "dom",
+        email: "dom@corvus.com" },
+      { userId: "71yYVDRUpOO8BR5153LMNCOmrV52",
+        name: "Boxcar",
+        location: "Boulder",
+        altitude: "5360",
+        equipment: "drum",
+        water: "calcium, flouride",
+        contact: "charles",
+        email: "charles@boxcar.com" }
+    ]
+
+    const wrapper = shallow(<CardContainer roasters={mockRoasters}/>)
+
+    expect(wrapper).toMatchSnapshot();
+  })
+})
