94c4d807027a71ada82aa337e0abec37b99cf192
Jordan Quinn <jpquinn605@gmail.com>
2018-02-26 09:06:20 -0700

    Lint first five components, Amy lint next 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from '../Main/Main';
 import { withRouter } from 'react-router-dom';
-import { logIn, retrievedRoasters, retrievedCoffees } from '../../actions/index';
+import { logIn, 
+  retrievedRoasters, 
+  retrievedCoffees } from '../../actions/index';
 import { connect } from 'react-redux';
-import { pullRoasters } from '../../Utilities/firebaseFunctions';
-import { firebaseApp } from '../../Utilities/firebaseFunctions';
+import { pullRoasters, firebaseApp } from '../../Utilities/firebaseFunctions';
 import PropTypes from 'prop-types';
 
 export const db = firebaseApp.database();
@@ -14,51 +15,51 @@ const ref2 = db.ref('/coffees');
 
 export class App extends Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       errorState: ''
-    }
+    };
   }
 
   async componentDidMount() {
-    try{
-      const user = JSON.parse(localStorage.getItem('user'))
+    try {
+      const user = JSON.parse(localStorage.getItem('user'));
       if (user) {
-       this.props.logIn(user);
-       const currentRoasters = await pullRoasters(ref1);
-       const currentCoffees = await pullRoasters(ref2);
-       localStorage.setItem('roasters', JSON.stringify(currentRoasters));
-       localStorage.setItem('coffees', JSON.stringify(currentCoffees));
-       this.props.retrievedRoasters(currentRoasters);
-       this.props.retrievedCoffees(currentCoffees);
+        this.props.logIn(user);
+        const currentRoasters = await pullRoasters(ref1);
+        const currentCoffees = await pullRoasters(ref2);
+        localStorage.setItem('roasters', JSON.stringify(currentRoasters));
+        localStorage.setItem('coffees', JSON.stringify(currentCoffees));
+        this.props.retrievedRoasters(currentRoasters);
+        this.props.retrievedCoffees(currentCoffees);
       }
     } catch (error) {
       this.setState({
         errorState: 'Error Retrieving Information'
-      })
+      });
     }
   }
 
   render() {
-    return(
+    return (
       <div> 
         <Main />
         <p> {this.state.errorState} </p>
       </div>
-    )
+    );
   }
 }
 
 export const mapStateToProps = state => ({
   user: state.user
-})
+});
 
 export const mapDispatchToProps = dispatch => ({
   logIn: user => dispatch(logIn(user)),
   retrievedRoasters: roasters => dispatch(retrievedRoasters(roasters)),
   retrievedCoffees: coffees => dispatch(retrievedCoffees(coffees))
-})
+});
 
 
 App.propTypes = {
